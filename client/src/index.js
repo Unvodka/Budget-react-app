3de1e4a9d95b7dc5e3255d12713102eb8221fe9f
arnaud chevallier <chevallier.a0@gmail.com>
2022-12-21 22:15:37 +0100

    Version 1.0.2

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { disableReactDevtools } from '@fvilers/disable-react-devtools';
-
-if (process.env.NODE_ENV === 'production') (disableReactDevtools())
 
 ReactDOM.render(
   <React.StrictMode>
