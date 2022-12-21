f9fd8cfad6508da984e0b2cdb9ee5ad25accb0cd
arnaud chevallier <chevallier.a0@gmail.com>
2022-12-21 22:10:56 +0100

    Version 1.0.1

--- client/src/api/base.js
+++ client/src/api/base.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-let BASE_URL  = "https://budget-react-app.onrender.com"
+// let BASE_URL  = "https://budget-react-app.onrender.com"
 
 if(process.env.NODE_ENV !== 'production') {
   BASE_URL = `http://localhost:3005`
