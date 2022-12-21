8e44c8cf6b888ea44540060e9fb54352ccaefe65
arnaud chevallier <chevallier.a0@gmail.com>
2022-12-22 00:05:50 +0100

    Version 1.0.8

--- config/index.js
+++ config/index.js
@@ -1,11 +1,9 @@
-const dotenv = require('dotenv');
+import { config } from 'dotenv';
 
-dotenv.config();
+config();
 
-module.exports = {
-  PORT: process.env.PORT,
-  MONGO_URI: process.env.MONGO_URI,
-  MONGO_DB_NAME: process.env.MONGO_DB_NAME,
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET
-};
+export const PORT = process.env.PORT;
+export const MONGO_URI = process.env.MONGO_URI;
+export const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
